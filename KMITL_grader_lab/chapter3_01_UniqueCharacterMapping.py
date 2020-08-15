9454d6996aa53e087121a6267b0b085d0b21341e
Sirawit <popeye-zz@hotmail.com>
2020-08-15 21:18:48 +0700

    thirdddd

---
+++ KMITL_grader_lab/chapter3_01_UniqueCharacterMapping.py
@@ -0,0 +1,23 @@
+string = input('Enter String : ')
+
+
+def mapping(str):
+    listCharacter = []
+    listNumber = []
+    position = 0
+    for i in range(len(str)):
+
+        for j in range(len(listCharacter)):
+            if listCharacter[j] == str[i]:
+                listNumber.append(listNumber[j])
+                break
+        else:
+            listNumber.append(position)
+            position += 1
+
+        listCharacter.append(str[i])
+
+    return listNumber
+
+
+print(mapping(string))
--- KMITL_grader_lab/chapter3_03_newRange.py
+++ KMITL_grader_lab/chapter3_03_newRange.py
@@ -1,8 +1,27 @@
-x, y, z = map(int, input().split())
-
-def oneFunction(x ,y, z):
-    pass
+print('*** New Range ***')
+listInput = [float(i) for i in input('Enter Input : ').split()]
 
 
-oneFunction(x,y,z)
+def oneFunction(ls):
+    listOutput = []
+    start = 0.0
+    stop = 0.0
+    step = 1.0
 
+    if len(listInput) == 1:
+        stop = ls[0]
+    elif len(listInput) == 2:
+        start, stop = ls[0], ls[1],
+    elif len(listInput) == 3:
+        start, stop, step = ls[0], ls[1], ls[2]
+    else:
+        return 'Error'
+
+    while start < stop:
+        listOutput.append(round(start, 3))
+        start += step
+
+    return tuple(listOutput)
+
+
+print(oneFunction(listInput))
