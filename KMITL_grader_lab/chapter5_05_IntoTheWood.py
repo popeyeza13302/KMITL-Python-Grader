62312a6b5e4d2bb22d774c6bd5d9b3d9e2d9a606
Sirawit <popeye-zz@hotmail.com>
2020-08-31 01:40:21 +0700

    finish week3 (Add comment)

--- KMITL_grader_lab/chapter5_05_IntoTheWood.py
+++ KMITL_grader_lab/chapter5_05_IntoTheWood.py
@@ -1,11 +1,11 @@
 class Stack:
     """< Default Stack >"""
 
-    def __init__(self, list=None):
-        if list == None:
+    def __init__(self, lst=None):
+        if lst is None:
             self.items = []
         else:
-            self.items = list
+            self.items = lst
 
     def __str__(self):
         s = 'stack of ' + str(self.size()) + ' items : '
@@ -35,48 +35,49 @@ class Stack:
 def reverseStack(old):
     new = Stack()
     while not old.isEmpty():
-        new.push(old.pop())  # REVERSE TREE TO MAIN
+        new.push(old.pop())  # REVERSE STACK
     return new
 
 
 inp = input('Enter Input : ').split(',')
 
-stk_tree = Stack()
+stk_tree = Stack()      # WE HAVE 3 STACKS
 stk_main = Stack()
 stk_see = Stack()
 
 for e in inp:
     e = e.split()
     if e[0] == 'A':
-        stk_tree.push(int(e[1]))
+        stk_tree.push(int(e[1]))    # PUSH NUM TO TREE
 
     elif e[0] == 'B':
         stk_main = reverseStack(stk_tree)  # REVERSE TREE TO MAIN
 
-        while not stk_main.isEmpty():  # RUN EVERY TREES
-            last = stk_main.pop()  # GET LAST_ONE OUT
+        while not stk_main.isEmpty():   # RUN EVERY TREES
+            last = stk_main.pop()           # GET LAST_ONE OUT
+            stk_tree.push(last)             # PUSH BACK TO TREE
 
-            stk_tree.push(last)  # PUSH TO TREE
-
-            if stk_see.isEmpty():
+            if stk_see.isEmpty():           # CHECK IF FIRST ONE (JUST THROW IN)
                 stk_see.push(last)
-                continue  # GO ON FOR FIRST_CASE
-            elif last < stk_see.peek():
-                stk_see.push(last)  # PUSH TO SEE
+                continue                    # SKIP
 
-            elif last >= stk_see.peek():
+            elif last < stk_see.peek():     # I'M SMALLER THAN YOU
+                stk_see.push(last)              # THROW TO SEE
 
-                while not stk_see.isEmpty() and last >= stk_see.peek():
-                    stk_see.pop()
+            elif last >= stk_see.peek():    # I'M BIGGER THAN YOU
+                while not stk_see.isEmpty() and last >= stk_see.peek():  # LOOP UNTIL I'M SMALLER THAN YOU
+                    stk_see.pop()           # DELETE OUT
                 else:
-                    stk_see.push(last)  # PUSH TO SEE
-        print(stk_see.size())
-        stk_see.empty()
+                    stk_see.push(last)          # PUSH TO SEE
+
+        print(stk_see.size())               # PRINT CAN SEE
+        stk_see.empty()                     # DELETE STACK OF SEE
 
     elif e[0] == 'S':
-        stk_main = reverseStack(stk_tree)  # REVERSE TREE TO MAIN
-        while not stk_main.isEmpty():
+        stk_main = reverseStack(stk_tree)   # REVERSE TREE TO MAIN
+
+        while not stk_main.isEmpty():       # RUN EVERY TREES
             if stk_main.peek() % 2 == 1:
-                stk_tree.push(stk_main.pop() + 2)
+                stk_tree.push(stk_main.pop() + 2)                   # ODD +2
             elif stk_main.peek() % 2 == 0 and stk_main.peek() > 0:
-                stk_tree.push(stk_main.pop() - 1)
+                stk_tree.push(stk_main.pop() - 1)                   # EVEN -1 (BUT GREATER THAN 0)
