cfa8ba0e8069f321c225e3752acfe2e21cba2eba
Sirawit <popeye-zz@hotmail.com>
2020-09-14 18:42:32 +0700

    in progress 5 week 6

--- KMITL_grader_lab/chapter6_05_RadixSort.py
+++ KMITL_grader_lab/chapter6_05_RadixSort.py
@@ -1,6 +1,6 @@
 # class Node
 class Node:
-    def __int__(self, data=None, next=None):
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
 
@@ -8,7 +8,7 @@ class Node:
         return str(self.data)
 
 
-class LinkedList:
+class SinglyLinkedList:
     def __init__(self):
         self.head = Node(None, None)
 
@@ -16,7 +16,9 @@ class LinkedList:
         s = ''
         p = self.head.next
         while p is not None:
-            s += str(p.data) + ' '
+            s += str(p.data)
+            if p.next is not None:
+                s += '->'
             p = p.next
         return s
 
@@ -28,15 +30,48 @@ class LinkedList:
             p = p.next
         return count
 
+    def isEmpty(self):
+        return self.size() == 0
 
-# 64 8 216 512 27 729 0 1 343 125
+    def nodeAt(self, index):    # -1 is dummy , 0 is first index
+        p = self.head           # dummy head
+        for _ in range(-1, index):
+            p = p.next
+        return p
+
+    def insert(self, index, data):
+        prevNode = self.nodeAt(index-1)
+        newNode = Node(data, prevNode.next)
+        prevNode.next = newNode
+
+    def append(self, data):
+        self.insert(self.size(), data)
+
+    def pop(self, index):
+        if self.isEmpty():             # check for case prevNode.next == None
+            return 'No more to pop'
+        if index > self.size()-1:
+            return 'out of range'
+
+        prevNode = self.nodeAt(index-1)
+        nextNode = prevNode.next.next   # no case None.next / but nextNode can be None
+        popNode = prevNode.next         # store popNode
+        prevNode.next = nextNode
+        return popNode  # return None or Node
+
+
+# 64 8 216 512 27 729 0 1 343 12
 
 inp = [int(i) for i in input('Enter Input : ').split()]
 
 time = 0
 
+L = SinglyLinkedList()
+
+for i in inp:
+    L.append(i)
 
 print(time, 'Time(s)')
-print('Before Radix Sort : ', None)
+print('Before Radix Sort : ', L)
 print('After  Radix Sort : ', None)
 
