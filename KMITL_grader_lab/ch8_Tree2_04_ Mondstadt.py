2c2842e29db7d18791d38dc9d7450bbf46d312bf
Sirawit <popeye-zz@hotmail.com>
2020-10-25 20:48:54 +0700

    in progress v.3 week 12

---
+++ KMITL_grader_lab/ch8_Tree2_04_ Mondstadt.py
@@ -0,0 +1,32 @@
+powerList, groupList = input('Enter Input : ').split('/')
+
+powerList = [int(i) for i in powerList.split()]
+groupList = [str(i) for i in groupList.split(',')]
+
+sumList = []
+
+
+def recursionTree(n):
+    sum = 0                             # initial sum = 0
+
+    if n >= len(powerList):             # if out of array
+        return 0
+
+    sum += recursionTree(2 * n + 1)     # go index left
+    sum += recursionTree(2 * n + 2)     # go index right
+    return powerList[n] + sum           # sum of (powerList[index] + sum)
+
+
+print(recursionTree(0))                 # root sum (index = 0)
+
+for i in groupList:
+    i = list(map(int, i.split()))       # 2 group compare
+    sum1 = recursionTree(i[0])          # group 1st
+    sum2 = recursionTree(i[1])          # group 2nd
+
+    if sum1 > sum2:
+        print(i[0], '>', i[1], sep='')
+    elif sum1 < sum2:
+        print(i[0], '<', i[1], sep='')
+    else:
+        print(i[0], '=', i[1], sep='')
